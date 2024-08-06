60caa5c383a9eba52d0ab6b84ce19c6b908805ef
Richard Velasco <rive4738@colorado.edu>
2024-08-06 00:47:55 -0700

    printing username

--- misc-modules/hello.c
+++ misc-modules/hello.c
@@ -8,7 +8,7 @@ MODULE_LICENSE("Dual BSD/GPL");
 static int hello_init(void)
 {
 	printk(KERN_ALERT "Hello, world\n");
-	printk(KERN_NOTICE "velascorboulder\n");
+	printk(KERN_ALERT "velascorboulder\n");
 	return 0;
 }
 
